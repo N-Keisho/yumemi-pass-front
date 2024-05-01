3d9bbe0629fe80a8128b8b8d202ed8251ed9a339
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2024-05-01 22:06:59 +0900

    chore: RESAS API

---
+++ types/resas.tsx
@@ -0,0 +1,37 @@
+// getPrefactures()の戻り値
+export interface Prefactures {
+    message: null;
+    result: Prefecture[];
+}
+
+// getPrefactures()の戻り値のresult
+export interface Prefecture {
+    prefCode: number;
+    prefName: string;
+}
+
+
+// getPopulation()の戻り値
+export interface Population {
+    message: null;
+    result: PopulationResult;
+}
+
+// getPopulation()の戻り値のresult
+export interface PopulationResult {
+    boundaryYear: number;
+    data: PopulationData[];
+}
+
+// getPopulation()の戻り値のresultのdata
+export interface PopulationData {
+    label: string;
+    data: PopulationDataDetail[];
+}
+
+// getPopulation()の戻り値のresultのdataのdata
+export interface PopulationDataDetail {
+    year: number;
+    value: number;
+}
+
