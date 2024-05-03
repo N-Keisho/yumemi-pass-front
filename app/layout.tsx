aad5a1a9fdc7e53f4947ea3dcb814e0ca1bb3188
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2024-05-04 00:05:58 +0900

    doc: Set Fotter Bottom

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./component/base/Header/Header";
+import Footer from "./component/base/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="jp">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
