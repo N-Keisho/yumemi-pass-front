f23fd9773f39f2a975f36065bffcb35c68516599
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2024-05-05 00:31:19 +0900

    test: Github Action Test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,6 @@
 describe("Navigation", () => {
   it("should navigate to the about page", () => {
     cy.visit("http://localhost:3000/");
-    cy.wait(1000);
     cy.contains("北海道").click();
     cy.contains("年少人口").click();
     cy.contains("生産年齢人口").click();
